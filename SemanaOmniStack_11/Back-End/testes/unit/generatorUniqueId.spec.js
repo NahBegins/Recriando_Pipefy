ce8d183ed22f4095b2451ca98192c52970beca14
Nathália Silva <souzanathalia048@gmail.com>
2020-04-01 15:59:29 -0300

    Delete generatorUniqueId.spec.js

--- SemanaOmniStack_11/Back-End/testes/unit/generatorUniqueId.spec.js
+++
@@ -1,8 +0,0 @@
-const generateUniqueId = require('../../src/utils/generatorUniqueId');
-
-describe('Generate Unique ID', () => {
-    it('should generate an unique ID', () => {
-        const id = generateUniqueId();
-        expect(id).toHaveLength(8);
-    });
-});
