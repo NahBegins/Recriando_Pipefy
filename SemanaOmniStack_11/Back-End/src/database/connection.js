8d11c31292f30d3ae44002091beb25b93356b381
Nathália Silva <souzanathalia048@gmail.com>
2020-04-01 15:58:04 -0300

    Delete connection.js

--- SemanaOmniStack_11/Back-End/src/database/connection.js
+++
@@ -1,9 +0,0 @@
-const knex = require('knex');
-const configuration = require('../../knexfile');
-
-// https://blog.rocketseat.com.br/variaveis-ambiente-nodejs/
-const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
-
-const connection = knex(config);
-
-module.exports = connection;
